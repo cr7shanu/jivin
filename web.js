d31879cf60e236538278a5f9f5ee347e1afdb733
cr7shanu <42079552+cr7shanu@users.noreply.github.com>
2018-08-12 14:33:28 -0700

    Add files via upload
    sab image ko images folder me daal diiegA
    >>DYNAMIC STAR RATING
    >>SEARCH BAR POSITION AND STYLING
    >>HOVER TO FOOTER SECTION BUTTON JAVASCRIPT

---
+++ web.js
@@ -0,0 +1,14 @@
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
+    }
+});
+$(document).ready(function(){
+    $("#button").click(function(){
+        $("#footer").show("slow");
+    });
+});
